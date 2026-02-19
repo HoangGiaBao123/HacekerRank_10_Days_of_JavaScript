260818953cd9ad1c84be7027f53c1c71fc16c6cd
Gia Bảo <hoanggiabao131012vn@gmail.com>
2026-02-19 08:48:15 +0700

    Create Day1_Arithmetic_Operators_.js

---
+++ Day1_Arithmetic_Operators_.js
@@ -0,0 +1,14 @@
+function getArea(length, width) {
+    let area;
+    area = length * width;
+    return area;
+}
+
+function getPerimeter(length, width) {
+    let perimeter;
+    perimeter = (length + width) * 2;
+    return perimeter;
+}
+
+print(getArea(6.1, 6.7))
+print(getPerimeter(21, 13)
