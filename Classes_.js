7fee08aed2435a4c496896c8f13724c1d4389db7
Gia Bảo <hoanggiabao131012vn@gmail.com>
2026-03-18 17:06:17 +0700

    Create Classes_.js

---
+++ Classes_.js
@@ -0,0 +1,20 @@
+class Polygon {
+    constructor(arr) {
+        this.lengths = arr;
+    }
+    perimeter() {
+        let result = 0;
+        for(let i = 0; i < this.lengths.length; i += 1) {
+            result += this.lengths[i];
+        }
+        return result;
+    } 
+}
+
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
